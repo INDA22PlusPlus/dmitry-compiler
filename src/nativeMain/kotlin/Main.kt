40546760b014cbaf76365923da3507bcdad1c4f5
Dmitry Chirin <chirin.dima@gmail.com>
2022-10-31 18:54:05 +0100

    implementing cases for lexer, add simple test string to tokenize

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,5 +1,5 @@
-open class Token(val str: String) {
-    open fun getStr(): String {
+open class Token(private val str: String) {
+    override fun toString(): String {
         return str
     }
 }
@@ -26,34 +26,71 @@ class CustomInt(str: String): Token(str) {
 
 
 class Lexer {
+    // TODO: Maps and filters instead of for loops
+    fun tokenize(str: String): MutableList<Token> {
+        println(str)
 
-//    fun tokenize(str: String): MutableList<Token> {
-//        val inp = str.split("\\s+".toRegex())
-//        println(inp)
-//
-//        val arr = mutableListOf<Token>()
-//
-//        for (e in inp) {
-//            if (e.matches("^(0|[1-9][0-9]*)\$".toRegex())) {
-//                arr.add(Token.INT)
-//            } else if (e.matches("\-".toRegex())) {
-//
-//            }
-//        }
-//
-//        println(arr)
-//
-//        return arr
-//    }
+        val lines = str.lines()
+//        var inp = str.split("\\s+".toRegex())
+        println(lines)
+
+        val arr = mutableListOf<Token>()
+
+        // Keywords
+
+        for (line in lines) {
+            val lineSplit = line.split("\\s+".toRegex())
+            for (token in lineSplit) {
+                when {
+                    // Keywords
+                    "if" == token -> arr.add(Keyword("if"))
+                    "else" == token -> arr.add(Keyword("else"))
+                    "elif" == token -> arr.add(Keyword("elif"))
+                    "when" == token -> arr.add(Keyword("when"))
+                    "print" == token -> arr.add(Keyword("print"))
+
+                    Regex("^(0|[1-9][0-9]*)\$").matches(token) -> arr.add(CustomInt(token))
+                }
+                print(token)
+                if (token.matches("^(0|[1-9][0-9]*)\$".toRegex())) {
+                    arr.add(CustomInt(token))
+                }
+            }
+            println()
+        }
+
+        println(arr)
+
+        return arr
+    }
 
 }
 
 fun main() {
-//    val l = Lexer()
-//    l.tokenize("0 + 1 + 1")
-    val t = Keyword("if")
-    println(t.getStr())
-
-    val t2 = CustomInt("1")
-    println(t2.getValue())
+    val l = Lexer()
+    l.tokenize("""
+        s = 0 + 1 + 2
+        b = 3
+        
+        if ( s > b ) {
+            print ( s )
+        } elif ( s > 4 ) {
+            print ( 5 )
+        } else {
+            print ( 6 )
+        }
+        
+        while ( s < b ) {
+            if b > s {
+                s = 7
+            }
+            s = s + 8
+        }
+    """.trimIndent()
+    )
+//    val t = Keyword("if")
+//    println(t)
+//
+//    val t2 = CustomInt("1")
+//    println(t2.getValue())
 }
