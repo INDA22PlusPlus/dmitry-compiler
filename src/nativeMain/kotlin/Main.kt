508ffa8c46b369757fbe03b9611b4bb74e8a984a
Dmitry Chirin <chirin.dima@gmail.com>
2022-10-31 21:22:44 +0100

    add new boilerplate classes and methods

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,5 +1,3 @@
-import platform.posix.log
-
 open class Token(private val str: String) {
     override fun toString(): String {
         return str
@@ -18,6 +16,11 @@ class SpecialCharacter(str: String): Token(str)
 
 class Variable(str: String): Token(str)
 
+class EOL(str: String): Token(str) {
+    override fun toString(): String {
+        return "\\n"
+    }
+}
 class CustomInt(str: String): Token(str) {
     private val value: Int = str.toInt()
 
@@ -27,51 +30,16 @@ class CustomInt(str: String): Token(str) {
 }
 
 
-class Lexer {
+class Lexer(fileName: String) {
+    val tokens: MutableList<Token>? = null
+    val tokensInRows: MutableList<MutableList<Token>>? = null
+    val sourceCode: String? = null
+
     // TODO: Maps and filters instead of for loops
     fun tokenize(str: String): MutableList<Token> {
-//        println(str)
-
-        val lines = str.lines()
-//        var inp = str.split("\\s+".toRegex())
-//        println(lines)
-
-        val arr = mutableListOf<Token>()
-
-        val keywords = listOf("if", "elif", "else", "while", "print")
-//        val mathOperators = listOf("+", "-", "*", "/", "**", "%", "//")        // Long form
-        val mathOperators = listOf("+", "-", "*", "/")                           // Short form
-//        val comparisonOperators = listOf("==", "!=", ">", "<", ">=", "<=")    // Long form
-        val comparisonOperators = listOf("==", "!=", ">", "<")                  // Short form
-        val logicOperators = listOf("&&", "||", "!")
-        val specialCharacters = listOf("(", ")", "{", "}", "=")
-
-//        val allTokenTemplates = keywords + mathOperators + comparisonOperators + logicOperators + specialCharacters
-
-        for (line in lines) {
-            val lineSplit = line.split("\\s+".toRegex())
-            for (token in lineSplit) {
-                when {
-                    keywords.contains(token) -> arr.add(Keyword(token))
-                    mathOperators.contains(token) -> arr.add(MathOperator(token))
-                    comparisonOperators.contains(token) -> arr.add(ComparisonOperator(token))
-                    logicOperators.contains(token) -> arr.add(LogicOperator(token))
-                    specialCharacters.contains(token) -> arr.add(SpecialCharacter(token))
-
-                    Regex("[a-zA-Z_]+").matches(token) -> arr.add(Variable(token))
-                    Regex("0|[1-9][0-9]*").matches(token) -> arr.add(CustomInt(token))
-                }
-//                print(token)
-//                if (token.matches("^(0|[1-9][0-9]*)\$".toRegex())) {
-//                    arr.add(CustomInt(token))
-//                }
-            }
-//            println()
-        }
-
-//        println(arr)
-
-        return arr
+        val tokens = mutableListOf<Token>()
+        tokenizeInRows(str).map { row -> tokens.addAll(row) }
+        return tokens
     }
     fun tokenizeInRows(str: String): MutableList<MutableList<Token>> {
 //        println(str)
@@ -80,7 +48,7 @@ class Lexer {
 //        var inp = str.split("\\s+".toRegex())
 //        println(lines)
 
-        val arr = mutableListOf<MutableList<Token>>()
+        val tokens = mutableListOf<MutableList<Token>>()
 
         val keywords = listOf("if", "elif", "else", "while", "print")
 //        val mathOperators = listOf("+", "-", "*", "/", "**", "%", "//")        // Long form
@@ -112,40 +80,55 @@ class Lexer {
 //                }
             }
             if (row.size > 0) {
-                arr.add(row)
+                row.add(EOL("\n"))
+                tokens.add(row)
             }
 //            println()
         }
 
 //        println(arr)
 
-        return arr
+        return tokens
+    }
+
+    fun printTokens() {
+
+    }
+
+    fun printTokensInRows() {
+
+    }
+
+    fun readFromFile() {
+
+    }
+
+    fun generateTokensFromSource() {
+
+    }
+
+    fun generateTokensInRowsFromSource() {
+
+    }
+
+    fun generateTokensFromFile() {
+
+    }
+
+    fun generateTokensInRowsFromFile() {
+
     }
 
 }
 
-fun main() {
-    val str = """
-        s = 0 + 1 + 2
-        b_ = 3
-        
-        if ( s > b ) {
-            print ( s )
-        } elif ( s > 4 ) {
-            print ( 5 )
-        } else {
-            print ( 6 )
-        }
-        
-        while ( s < b ) {
-            if b > s {
-                s = 7
-            }
-            s = s + 8
-        }
-    """.trimIndent()
 
-    val l = Lexer()
+class Parser {
+
+}
+
+class Compiler(val lexer: Lexer, val parser: Parser)
+fun main() {
+    val l = Lexer("source.dc")
     val tokens = l.tokenizeInRows(str)
     tokens.map { row ->
         row.map { token -> print("$token ") }
