02dab5e1c07b3bb79338ddac39c85b50a8814517
Dmitry Chirin <chirin.dima@gmail.com>
2022-11-04 02:11:54 +0100

    update to fit os

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,10 +7,22 @@ fun main(args: Array<String>) {
 //    println(compiler.compileFromSourceToString(args[0]))
     compiler.compileFromSourceToFile(args[0], args[1])
 //    Runtime.getRuntime().exec("python fib.py")
-    ProcessBuilder("cmd", "/C", "python ${args[1]}")
-        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-        .start()
-        .waitFor()
+    val os = System.getProperty("os.name").lowercase()
+    when {
+        os.contains("win") -> {
+            ProcessBuilder("cmd", "/C", "python ${args[1]}")
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor()
+        }
+
+        os.contains("mac") -> {
+            ProcessBuilder("python3 ${args[1]}")
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor()
+        }
+    }
 
 
 //    val comparison = Comparison.getComparisonFromTokens(compiler.lexer.tokenize("a + 3 == 3 * b"))
